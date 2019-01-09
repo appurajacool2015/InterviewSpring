1fd07ba08a4df73ab9feab590cf27ecd4588ac96
appuraja <appurajacool2015@gmail.com>
2019-01-09 22:48:21 +0530

    Adding README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+####################
+This repo hold each sample under each package.
+Eg. 
+
+#1 
+For Comparator sample we can go to 'com.anand.Comparator' package
+ and there will a main method for testing.
+
+#2
+Same for the bean life cycle - com.anand.beanlifecycle package
+Run the application InterviewSpringApplication as Spring boot Application / Java application.
+
+ 
--- src/main/java/com/anand/beanlifecycle/InterviewSpringApplication.java
+++ src/main/java/com/anand/beanlifecycle/InterviewSpringApplication.java
@@ -3,20 +3,21 @@ package com.anand.beanlifecycle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.anand.beanlifecycle.ElephantService;
+import com.anand.beanlifecycle.AppConfig;
 
 @SpringBootApplication
-public class BeanpostprocessorApplication {
+public class InterviewSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeanpostprocessorApplication.class, args);
-
+		SpringApplication.run(InterviewSpringApplication.class, args);
+		
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 	    ctx.register(AppConfig.class);
 	    ctx.refresh();
 	    ElephantService service = ctx.getBean(ElephantService.class);
 		service.print();
     	ctx.registerShutdownHook();
-
 	}
 
 }
